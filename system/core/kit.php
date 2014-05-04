9059f4b3494f4e51245c157f3398e24bafed174e
Orel Hadad <orelhadad@gmail.com>
2014-05-04 10:55:14 +0300

    Set function in Kit object to get URL parts

--- system/core/kit.php
+++ system/core/kit.php
@@ -3,7 +3,27 @@ spl_autoload_extensions('.php');
 spl_autoload_register();
 
 final class Kit{
+    private $UrlParts = false;
+    private $Controller = false;
+    private $Class = false;
+    private $Method = false;
+    private $Errors = false;
+    private $ErrorPage = false;
+    private $Config = false;
+    private $AutoLoad = false;
+    private $Router = false;
+
     function __construct(){
-        new System\Core\Controllers();
+        $this->getUrlParts();
     }
+
+    private function getUrlParts(){
+        $accessPath = (isset($_SERVER['PATH_INFO']))? $_SERVER['PATH_INFO']:'/';
+        $accessPath = explode('/', $accessPath);
+        array_shift($accessPath);
+        if($accessPath[0] == NULL) array_shift($accessPath);
+        if(!count($accessPath)) return false;
+        $this->UrlParts = $accessPath;
+    }
+    // Divider PATH_INFO to array by `/` into $this->_UrlParts.
 }
