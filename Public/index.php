3aa280349a4edc814f8aaf9537d3180dd885911d
Orel Hadad <orel@lok.team>
2016-08-01 17:28:15 +0300

    fix

--- Public/index.php
+++ Public/index.php
@@ -4,6 +4,13 @@ define('BASE_PATH', dirname(__DIR__).DIRECTORY_SEPARATOR);
 
 require_once BASE_PATH.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
-$app = new Kit\Core\System();
+use Kit\Core\System;
+
+if(isset($argv))
+	System::setArgv($argv);
+
+$app = new System();
 
 $app->run();
+
+echo "\n";
